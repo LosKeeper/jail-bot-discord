747e5fd08079b892e1611d7a0383bbb03c48f61b
unknown <thomas.dumond2001@gmail.com>
2022-05-02 18:27:11 +0200

    discord.py shit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Jail bot
-Add auto reaction to people
+Add reaction to people with a specific user id. If the 🥜 emoji is has more than 9 reactions, the bot will timeout the specific user.
+
+## Configuration
+You need to ass a `config.py` file in the same directory as the bot.
+
+```python
+token = 'your_token_here'
+uid = 'the user id of the person you want to bully'
+```
+
+You also need some libraries which you can install with :
+
+```pwsh
+pip install -r requirements.txt
+```
--- jail-bot.py
+++ jail-bot.py
@@ -1,6 +1,8 @@
 from config import token, uid
 import discord
 from discord.ext import commands
+from discord.utils import get
+import asyncio
 
 bot = commands.Bot(command_prefix='!')
 
@@ -8,15 +10,44 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print("Logged in as", bot.user.name)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="everyone sending peanuts"))
 
 
 @bot.event
 async def on_message(message):
-    # if user send a message
-
     if(message.author.id == uid):
         await message.add_reaction('⬆️')
         await message.add_reaction('🥜')
         await message.add_reaction('⬇️')
+
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    if payload.emoji.name == '🥜':
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        if message.author.id == uid:
+            reaction = get(message.reactions, emoji=payload.emoji.name)
+            if reaction and reaction.count == 2:
+                member = discord.Member
+                member.id = uid
+                await mute(channel, member, 10)
+
+
+# mute command
+async def mute(ctx, member: discord.Member, mute_time: int):
+    if not member:
+        await ctx.send("Who do you want me to mute?")
+        return
+    muted = get(ctx.guild.roles, id=970710834105950288)
+    membre = get(ctx.guild.roles, id=885801392483229727)
+    await member.add_roles(muted)
+    await member.remove_roles(membre)
+    await ctx.send("{} has been muted for {} seconds".format(member.mention, mute_time))
+
+    await asyncio.sleep(mute_time)
+    await member.remove_roles(muted)
+    await member.add_roles(membre)
+    await ctx.send("{} has been unmuted".format(member.mention))
 
 bot.run(token)
